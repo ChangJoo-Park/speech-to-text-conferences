2cefe3aeee634c49418f8e6cba555ebf725128d3
ChangJoo Park <pcjpcj2@gmail.com>
2018-05-23 04:17:29 +0900

    Scaffolding :)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,107 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 대화 가능 화면 -->
+    <div
+      v-if="currentUser.isIn"
+      class="speech-conference"
+    >
+      <div>
+        <strong>{{ currentUser.name }}</strong>
+      </div>
+      <input
+        type="button"
+        @mousedown="onStartSpeech"
+        @mouseup="onStopSpeech"
+        :value="currentUser.isSpeaking ? '놓아서 멈추세요' : '눌러서 시작하세요'">
+      <br>
+      <input type="button" @click="leaveRoom" value="나가기">
+      <div>
+        <div
+          v-for="(sentence, index) in sentences"
+          :key="index"
+        >
+          {{ sentence }}
+        </div>
+      </div>
+    </div>
+    <!-- 진입 화면 -->
+    <div
+      v-else
+      class="form-join-room"
+    >
+      <form @submit.prevent="joinRoom">
+        <input
+          type="text"
+          placeholder="이름을 입력하세요"
+          required
+          v-model="newName"
+        >
+        <input type="submit">
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      isSupportAnnyang: false,
+      newName: 'John Doe',
+      currentUser: {
+        name: null,
+        isIn: false,
+        isSpeaking: false
+      },
+      sentences: []
+    }
+  },
+  mounted () {
+    /* global annyang */
+    this.isSupportAnnyang = !!annyang
+    if (this.isSupportAnnyang) {
+      annyang.setLanguage('ko')
+      annyang.addCallback('start', this.startSpeech)
+      annyang.addCallback('end', this.endSpeech)
+      annyang.addCallback('result', this.processResultSpeech)
+    }
+  },
+  beforeDestroy () {
+    if (annyang) {
+      annyang.removeCallback()
+    }
+  },
+  methods: {
+    startSpeech () {
+      this.currentUser.isSpeaking = true
+    },
+    endSpeech () {
+      this.currentUser.isSpeaking = false
+    },
+    processResultSpeech (resultText) {
+      console.log(resultText)
+    },
+    onStartSpeech () {
+      if (this.isSupportAnnyang) {
+        annyang.start({ autoRestart: false, paused: true })
+      }
+    },
+    onStopSpeech () {
+      if (this.isSupportAnnyang) {
+        annyang.abort()
+      }
+    },
+    leaveRoom () {
+      this.currentUser.name = ''
+      this.currentUser.isIn = false
+      this.currentUser.isSpeaking = false
+      this.newName = ''
+    },
+    joinRoom () {
+      this.currentUser.name = this.newName
+      this.currentUser.isIn = true
+    }
   }
 }
 </script>
